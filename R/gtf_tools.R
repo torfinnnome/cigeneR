c86168db465f5282b49a314e6dbda8a2119f1f0e
Tim Knutsen <timknutsen@gmail.com>
2015-03-25 07:36:15 +0100

    update documentation
    replae ids working before update

--- R/gtf_tools.R
+++ R/gtf_tools.R
@@ -1,11 +1,10 @@
+library(devtools)
 #' Reads GTFs
 #'
 #' Does what is says.
 #' @param filename Path to gtf file.
 #' @keywords GTF
 #' @export
-#' @examples
-#' gtf_reader()
 gtf_reader <- function(filename) {
 	dplyr::tbl_df(data.table::fread(filename, data.table = F))
 }
@@ -17,39 +16,40 @@ gtf_reader <- function(filename) {
 #' @param filename new gtf filename.
 #' @keywords GTF
 #' @export
-#' @examples gtf_writer()
 gtf_writer <- function(x, filename) {
 	write.table(x, file = filename, row.names = F, col.names = F, quote = F, sep = "\t")
 }
 
-#' Removes unstranded from gtf
+#' Removes unstranded from gtf read into R.
 #'
-#' Does what is says.
+#' Assumes stranded info is in column 7.
 #' @param x GTF object.
 #' @keywords GTF
 #' @export
 #' @examples
-#' gtf_reader()
+#' remove_unstranded(mtcars)
 
 remove_unstranded <- function(x) {
-	dplyr::filter(x, V7 != ".")
+	col7 <- colnames(x)[7]
+	dplyr::filter(x, col7 != ".")
 }
 
-#' Update id in plink ped
+#' Update id in vector of IDs, eg. plink fam.
 #'
-#' Update Ids in plink ped file based on two column data frame
-#' @param x Character vector of lines from ped
-#' @param List two column text file with pattern and replacement
+#' Update Ids in dataframe column based on two column data frame
+#' @param x Character vector of IDs to update.
+#' @param List two column dataframe with pattern and replacement
 #' @keywords plink
 #' @export
 #' @examples
-#' replace_id_str()
+#'
+#' replace_id_str(x)
 
 replace_id_str <- function(x, list){
-	assertthat::assert_that(ncol(id_changes_fattyacid) == 2)
+	assertthat::assert_that(ncol(list) == 2)
 	names(list) <- c("V1", "V2")
 	pattern = paste("\\b", list$V1, "\\b", sep = "") # only 1606 and not eg. 01606 is replaced.
 	replacement = list$V2
-	stringi::stri_replace_all(
+		stringi::stri_replace_all(
 		str = x, replacement = replacement, regex = pattern, vectorize_all = F)
 }
